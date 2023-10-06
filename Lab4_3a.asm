031a41d6e4b3edcbddd72cd938e27a6b31f41d5f
CKelsey95 <43053498+CKelsey95@users.noreply.github.com>
2023-10-06 03:47:53 -0600

    Update Lab4_3a.asm
    functions

--- Lab4_3a.asm
+++ Lab4_3a.asm
@@ -1,24 +1,34 @@
 .data
-x: li $t1, 2
 str1: .asciiz "the value of X before subfun is: "
 str2: .asciiz "the value of X after subfun is: "
+newline: .asciiz "\n"
+x: .word 2
+
 .text
 main:
-	la $a0, str1
+        lw $t1, x   # loads value of x into $t1
+        
+	la $a0, str1  # prints string 1
 	li $v0, 4
-	la $a0, x
-	li $v0, 1
 	syscall
 	
-	jal subfunc
+	lw $a0, x
+	li $v0, 1   # prints initial value of X
+	syscall
 	
-	li $v0, 4
+	li $v0, 4       
+	la $a0, newline  # prints a newline
+	syscall
+	
+	jal subfunc  # jumps to subfunction (+5 to X)
+	
 	la $a0, str2
+	li $v0, 4       # prints string 2
+	syscall
+	
 	li $v0, 1
-	la $a0, x
+	move $a0, $t1     # prints new value of X (7)
 	syscall
 	
-	li $v0, 10
+	li $v0, 10      # ends program
 	syscall
-	
-	
